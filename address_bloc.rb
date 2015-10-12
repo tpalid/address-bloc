ce904f65234a0b2dedd7fe6c1dcc8c3a244f37fb
Theresa Palid <tpalid1@gmail.com>
2015-10-12 01:09:06 +0000

    Revert "Assignment 24 Complete"
    This reverts commit 00fc836c6c129ac9452ac2521559b8da90408ac1.

--- address_bloc.rb
+++ address_bloc.rb
@@ -1,14 +1,1 @@
 puts "Welcome to AddressBloc!"
-
-#driver program
-
-require_relative "controllers/menu_controller"
-
-menu = MenuController.new
-
-system "clear"
-    puts "Welcome to AddressBloc!"
-    
-menu.main_menu
-
-#main_menu is defined in menu_controller
